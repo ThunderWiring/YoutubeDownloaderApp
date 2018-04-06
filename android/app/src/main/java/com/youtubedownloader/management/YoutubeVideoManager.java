d932d1311c7de019d467baec25cf7930c069202b
Bassam <bassam.yasin@gmail.com>
2018-04-06 11:03:10 +0300

    added managment dir for handling video monitoring

--- android/app/src/main/java/com/youtubedownloader/management/YoutubeVideoManager.java
+++ android/app/src/main/java/com/youtubedownloader/management/YoutubeVideoManager.java
@@ -1,13 +1,8 @@
 package com.youtubedownloader.management;
 
-import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.Video;
 public class YoutubeVideoManager {
-    private YouTube youTube;
-    private Video vid;
 
     public YoutubeVideoManager() {
-//        youTube = new YouTube();
-        vid.getId();
+
     }
 }
