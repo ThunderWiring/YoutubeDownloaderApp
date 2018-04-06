4480da7eec3fee82b459862ee96db874e635c6d1
Bassam <bassam.yasin@gmail.com>
2018-04-06 10:59:55 +0300

    Resources files updated

---
+++ android/app/src/main/java/com/youtubedownloader/management/YoutubeVideoManager.java
@@ -0,0 +1,13 @@
+package com.youtubedownloader.management;
+
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.Video;
+public class YoutubeVideoManager {
+    private YouTube youTube;
+    private Video vid;
+
+    public YoutubeVideoManager() {
+//        youTube = new YouTube();
+        vid.getId();
+    }
+}
